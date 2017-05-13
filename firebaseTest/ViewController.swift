e1f4d9cbbc099c59f834fa42ea6074397dd0b56d
HyunsooPark <qkrgustnrk@gmail.com>
2017-05-13 20:00:12 +0900

    Still need to modify chat bubble

--- firebaseTest/ViewController.swift
+++ firebaseTest/ViewController.swift
@@ -8,13 +8,14 @@
 
 import UIKit
 
-// 1. 내가 작성한 내용이 파이어베이스에 저장되게 한다.
-class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+class ViewController: UIViewController, UITextFieldDelegate, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    
     @IBOutlet weak var item: UINavigationItem!
     
     var groupKey: String? {
         didSet {
             if let key = groupKey {
+                fetchMessages()
                 FirebaseDataService.instance.groupRef.child(key).observeSingleEvent(of: .value, with: { (snapshot) in
                     if let data = snapshot.value as? Dictionary<String, AnyObject> {
                         if let title = data["name"] as? String {
@@ -26,31 +27,50 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
         }
     }
     
+    var messages: [ChatMessage] = [ChatMessage(fromUserId: "", text: "", timestamp: 0)]
+    
     @IBOutlet weak var chatCollectionView: UICollectionView!
     @IBOutlet weak var sendButton: UIButton!
     @IBOutlet weak var chatTextField: UITextField!
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        chatCollectionView.delegate = self
-        chatCollectionView.dataSource = self
-    }
-    
     // numberOfItemsInSection
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 0
+        return messages.count
     }
     
     // cellForItemAt
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        return UICollectionViewCell()
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "chatCell", for: indexPath) as! ChatMessageCell
+        let message = messages[indexPath.item]
+        cell.textLabel.text = message.text
+        setupChatCell(cell: cell, message: message)
+        if message.text.characters.count > 0 {
+            cell.containerViewWidthAnchor?.constant = measuredFrameHeightForEachMessage(message: message.text).width + 32
+        }
+        return cell
     }
     
     // sizeForItemAt
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return CGSize(width: view.frame.size.width, height: 140)
+    func setupChatCell(cell: ChatMessageCell, message: ChatMessage) {
+        if message.fromUserId == FirebaseDataService.instance.currentUserUid {
+            cell.containerView.backgroundColor = UIColor.magenta
+            cell.textLabel.textColor = UIColor.white
+            cell.containerViewRightAnchor?.isActive = true
+            cell.containerViewLeftAnchor?.isActive = false
+        } else {
+            cell.containerView.backgroundColor = UIColor.lightGray
+            cell.textLabel.textColor = UIColor.black
+            cell.containerViewRightAnchor?.isActive = false
+            cell.containerViewLeftAnchor?.isActive = true
+        }
     }
-
+    
+    private func measuredFrameHeightForEachMessage(message: String) -> CGRect {
+        let size = CGSize(width: 200, height: 1000)
+        let options = NSStringDrawingOptions.usesFontLeading.union(.usesLineFragmentOrigin)
+        return NSString(string: message).boundingRect(with: size, options: options, attributes: [NSFontAttributeName: UIFont.systemFont(ofSize: 16)], context: nil)
+    }
+    
     @IBAction func sendButtonTapped(_ sender: UIButton) {
         let ref = FirebaseDataService.instance.messageRef.childByAutoId()
         guard let fromUserId = FirebaseDataService.instance.currentUserUid else {
@@ -75,5 +95,106 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
             }
         }
     }
-}
+    
+    @IBAction func collectionViewTapped(_ sender: Any) {
+        chatTextField.resignFirstResponder()
+    }
+    
+    func fetchMessages() {
+        if let groupId = self.groupKey {
+            let groupMessageRef = FirebaseDataService.instance.groupRef.child(groupId).child("messages")
+            groupMessageRef.observe(.childAdded, with: { (snapshot) in
+                let messageId = snapshot.key
+                let messageRef = FirebaseDataService.instance.messageRef.child(messageId)
+                messageRef.observeSingleEvent(of: .value, with: { (snapshot) in
+                    guard let dict = snapshot.value as? Dictionary<String, AnyObject> else {
+                        return
+                    }
+                    let message = ChatMessage(
+                        fromUserId: dict["fromUserId"] as! String,
+                        text: dict["text"] as! String,
+                        timestamp: dict["timestamp"] as! NSNumber
+                    )
+                    self.messages.insert(message, at: self.messages.count - 1)
+                    self.chatCollectionView.reloadData()
+                    if self.messages.count >= 1 {
+                        let indexPath = IndexPath(item: self.messages.count - 1, section: 0)
+                        self.chatCollectionView.scrollToItem(at: indexPath, at: UICollectionViewScrollPosition(), animated: true)
+                    }
+                })
+            })
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        chatCollectionView.delegate = self
+        chatCollectionView.dataSource = self
+        chatTextField.delegate = self
+        let layout = chatCollectionView.collectionViewLayout as! UICollectionViewFlowLayout
+        layout.estimatedItemSize.width = view.frame.width
+        chatCollectionView.alwaysBounceVertical = true
+        sendButton.isEnabled = false
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        subscribeToKeyboardNotifications()
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        unsubscribeFromKeyboardNotifications()
+    }
+    
+    // get keyboard height and shift the view from bottom to higher
+    func keyboardWillShow(_ notification: Notification) {
+        if chatTextField.isFirstResponder {
+            view.frame.origin.y = -getKeyboardHeight(notification)
+        }
+    }
+    
+    func keyboardWillHide(_ notification: Notification) {
+        if chatTextField.isFirstResponder {
+            view.frame.origin.y = 0
+        }
+    }
+    
+    func getKeyboardHeight(_ notification: Notification) -> CGFloat {
+        let userInfo = notification.userInfo
+        let keyboardSize = userInfo![UIKeyboardFrameEndUserInfoKey] as! NSValue
+        return keyboardSize.cgRectValue.height
+    }
+    
+    func subscribeToKeyboardNotifications() {
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(_:)), name: .UIKeyboardWillShow, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(_:)), name: .UIKeyboardWillHide, object: nil)
+    }
+    
+    func unsubscribeFromKeyboardNotifications() {
+        NotificationCenter.default.removeObserver(self, name: .UIKeyboardWillShow, object: nil)
+        NotificationCenter.default.removeObserver(self, name: .UIKeyboardWillHide, object: nil)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        chatTextField.resignFirstResponder()
+        return true
+    }
+    
+    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+        chatCollectionView.collectionViewLayout.invalidateLayout()
+    }
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        var newText: NSString = textField.text! as NSString
+        newText = newText.replacingCharacters(in: range, with: string) as NSString
+        
+        if newText.length < 1 {
+            sendButton.isEnabled = false
+        } else {
+            sendButton.isEnabled = true
+        }
+        return true
+    }
 
+}
